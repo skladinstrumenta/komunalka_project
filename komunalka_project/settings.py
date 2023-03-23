9c07ea04face925d7027408e66e863bd155372b2
Alexandr <skladinstrumenta@gmail.com>
2023-03-23 21:45:05 +0200

    add KomunalDataListView

--- komunalka_project/settings.py
+++ komunalka_project/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'app_komunalka.MyUser'
+
+# LOGIN_REDIRECT_URL = '/'
+
+TIME_ZONE = 'Europe/Kyiv'
